902ba20b7d2930e375923fb09bdc18e350034460
apoliras <77238509+apoliras@users.noreply.github.com>
2021-12-12 23:40:41 +0200

    Add files via upload

---
+++ apo.cluster/cube_funcs.h
@@ -0,0 +1,7 @@
+#ifndef CUBE_FUNCS
+
+int * h_p(int**, int,int);
+int con(int, int);
+int hammingDistance(int, int);
+
+#endif
---
+++ apo.cluster/lsh_funcs.h
@@ -0,0 +1,13 @@
+#ifndef LSH_FUNCS
+
+struct Hash_Node
+{
+    char* name;
+    int item;
+    int ID;
+    struct Hash_Node* next;
+};
+
+float h_function(double**, int, int);
+
+#endif
